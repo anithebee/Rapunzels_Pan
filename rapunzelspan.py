46f172923f39eb162008a18a8f1bd6b3503b0624
anithebee <143959897+anithebee@users.noreply.github.com>
2024-02-08 10:19:24 +0530

    Update rapunzelspan.py

--- rapunzelspan.py
+++ rapunzelspan.py
@@ -230,8 +230,8 @@ st.divider()
 #dish ideas
 dishideas = st.button("Dish Ideas", help="Stuck with random ingredients? Find recipe ideas!")
 dishinputnum = st.number_input("Enter the number of ingredients you have: ", step=1)
+ingredients = []
 if dishinputnum:
-    ingredients = []
     for i in range(dishinputnum):
         ingredient = st.text_input(f"Enter ingredient #{i + 1}: ")
         ingredients.append(ingredient)
