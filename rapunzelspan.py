6a32b4698cd810cec696b68469a3333465b05ac6
anithebee <143959897+anithebee@users.noreply.github.com>
2024-02-08 10:17:11 +0530

    Update rapunzelspan.py

--- rapunzelspan.py
+++ rapunzelspan.py
@@ -233,8 +233,8 @@ dishinputnum = st.number_input("Enter the number of ingredients you have: ", ste
 if dishinputnum:
     ingredients = []
     for i in range(dishinputnum):
-    ingredient = st.text_input(f"Enter ingredient #{i + 1}: ")
-    ingredients.append(ingredient)
+        ingredient = st.text_input(f"Enter ingredient #{i + 1}: ")
+        ingredients.append(ingredient)
 for ingredient in ingredients:
     st.write(f"\nRecipe using {ingredient}:")  # Changed "for" to "using"
     for website in websites:
